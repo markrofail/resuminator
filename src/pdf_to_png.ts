e6a38f2f8a751cffe89818e515a04845fc4237b6
Mark Rofail <markm.rofail@gmail.com>
2024-09-27 09:38:02 +0100

    [feat] add timestamp to generated file

--- src/pdf_to_png.ts
+++ src/pdf_to_png.ts
@@ -1,9 +1,12 @@
+import fs from "fs"
 import path from "path"
 import { pdfToPng } from "pdf-to-png-converter"
 
 const ROOT_DIR = path.join(__dirname, "..")
 
-pdfToPng(path.join(ROOT_DIR, "output/markrofail-cv.pdf"), {
-    viewportScale: 2.0,
-    outputFolder: path.join(ROOT_DIR, "output"),
+fs.readdirSync(path.join(ROOT_DIR, "output")).forEach(async (file) => {
+    if (!file.endsWith(".pdf")) return
+
+    const [pngFile] = await pdfToPng(path.join(ROOT_DIR, "output", file), { viewportScale: 2.0 })
+    fs.writeFileSync(path.join(ROOT_DIR, "output/preview.png"), pngFile.content)
 })
